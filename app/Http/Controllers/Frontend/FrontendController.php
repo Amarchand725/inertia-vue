ceb4b79a69d417d2ec3f58f21bc868c9644a32d4
Developer <chandamar725@gmail.com>
2023-02-16 15:21:07 +0500

    first commit

---
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+
+class FrontendController extends Controller
+{
+    public function index(){
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
+}
